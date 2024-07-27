aa5d7df67aadb1e0efa03bc350a31733d229965c
Team403 <55448414+tripraptomo@users.noreply.github.com>
2024-07-27 11:29:43 +0700

    Update script.js

--- daftar/script.js
+++ daftar/script.js
@@ -3,5 +3,5 @@ const loadingTime = 2000; // 2 detik
 
 // Setelah loading selesai, redirect ke halaman lain
 setTimeout(() => {
-    window.location.href = "halaman_tujuan.html"; // Ganti dengan URL yang ingin dituju
+    window.location.href = "https://loading-ashy.vercel.app/daftar"; // Ganti dengan URL yang ingin dituju
 }, loadingTime);
