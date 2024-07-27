598e46a8d77322c607c9060a9610a3fff9c0fa8c
Team403 <55448414+tripraptomo@users.noreply.github.com>
2024-07-27 11:28:17 +0700

    Create script.js

---
+++ daftar/script.js
@@ -0,0 +1,7 @@
+// Simulasi waktu loading (sesuaikan dengan kebutuhan)
+const loadingTime = 2000; // 2 detik
+
+// Setelah loading selesai, redirect ke halaman lain
+setTimeout(() => {
+    window.location.href = "halaman_tujuan.html"; // Ganti dengan URL yang ingin dituju
+}, loadingTime);
